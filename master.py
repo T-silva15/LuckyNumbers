83e3e7a6ddcfa8726ae2978e02e4a494a990b4da
T-silva15 <128994723+T-silva15@users.noreply.github.com>
2023-05-09 14:21:12 +0100

    Add files via upload

---
+++ master.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import tkinter.font as font
+from turtle import bgcolor
+from PIL import ImageTk, Image
+import random
+import tkinter.messagebox
+
+#window configuration
+window = Tk()
+window.title("Lucky Numbers") 
+window.iconbitmap('C:\Source\LabProg\LuckyNumbers\imageFiles\icon.ico')
+window.configure(bg='#8beae9')
+window.geometry('1000x600')
+
+
+
+
+window.mainloop()
