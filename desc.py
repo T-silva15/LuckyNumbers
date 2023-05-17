199316576b1db6ad7aa4cfed66ec9adf82e45901
Tiago Silva <tiagoissilva@hotmail,com>
2023-05-17 10:01:47 +0100

    Brief description of the game

--- desc.py
+++ desc.py
@@ -4,4 +4,5 @@ def showDescription():
     os.system('cls')
     print('\n O Lucky Numbers é um jogo de tabuleiro no qual o jogador precisa de formar uma sequência crescente\n de números, tanto na horizontal quanto na vertical. A cada jogada é fornecido um novo número, exceto\n quando o jogador não coloca o número atribuído na jogada anterior ou troca com um número já presente\n no tabuleiro. Quando isso acontece, o número substituído é colocado em uma mesa separada, que\n ambos os jogadores têm acesso e podem utilizar para futuras trocas, caso seja conveniente.\n')
     input("Pressione uma tecla para continuar...")
-    os.system('cls')          
+    os.system('cls') 
+         
