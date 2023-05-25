c82bf16ea048fd2bb8460802b624404b24bdff46
Tiago Silva <tiagoissilva@hotmail,com>
2023-05-25 16:11:09 +0100

    Implemented some color changes on the menus, fixed some input errors

--- main.py
+++ main.py
@@ -14,46 +14,58 @@ def showPage():
     # LuckyNumbers logo in AXCII
     print(f"{Fore.GREEN}\n ___      __   __  _______  ___   _  __   __    __    _  __   __  __   __  _______  _______  ______    _______ \n|   |    |  | |  ||       ||   | | ||  | |  |  |  |  | ||  | |  ||  |_|  ||  _    ||       ||    _ |  |       |\n|   |    |  | |  ||       ||   |_| ||  |_|  |  |   |_| ||  | |  ||       || |_|   ||    ___||   | ||  |  _____|\n|   |    |  |_|  ||       ||      _||       |  |       ||  |_|  ||       ||       ||   |___ |   |_||_ | |_____ \n|   |___ |       ||      _||     |_ |_     _|  |  _    ||       ||       ||  _   | |    ___||    __  ||_____  |\n|       ||       ||     |_ |    _  |  |   |    | | |   ||       || ||_|| || |_|   ||   |___ |   |  | | _____| |\n|_______||_______||_______||___| |_|  |___|    |_|  |__||_______||_|   |_||_______||_______||___|  |_||_______|")
     # authors of the game
-    print(f"\n-------------------------------------{Back.WHITE}{Fore.BLACK}Jogo de: Filipe Nunes e Tiago Isidro{Back.RESET}{Fore.RESET}-------------------------------------")
+    print(f"\n{Fore.RED}-------------------------------------{Back.WHITE}{Fore.BLACK}Jogo de: Filipe Nunes e Tiago Isidro{Back.RESET}{Fore.RED}-------------------------------------")
     input("\nPressione uma tecla para avançar...")
 
 # Player initialization
-player1 = ""
+player1 = " "
 board1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 # create bot 
-player2 = "BOT"
+player2 = " "
 board2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+def chooseGame():
+    os.system('cls')
+    while True:         # loops until valid answer
+        os.system('cls')
+        # asks user for the game he wants to play
+        print(f"{Fore.RED}1. {Fore.WHITE} Jogar com dois Jogadores")
+        print(f"{Fore.RED}2. {Fore.WHITE} Jogar contra um Bot")
+        x = input(f"{Fore.GREEN}Introduza uma opção: ")
+        if x in ['1','2']:
+            x = int(x)
+            break
+    return x            # returns user decision
+    
 # window configuration
 def showMenu():
-    # menu creation
-    os.system('cls')
-    print(f"| / / / / / / / / / /{Fore.GREEN}🍀Lucky Numbers🍀{Fore.WHITE}/ / / / / / / / / / |")
-    print(f"| / / / / / / / / / /       MENU      / / / / / / / / / / |")
-    print("|                                                         |")
-    print(f"| {Fore.RED}1. {Fore.WHITE}Iniciar uma partida                                  |")
-    print(f"| {Fore.RED}2. {Fore.WHITE}Carregar uma partida a partir de um ficheiro         |")
-    print(f"| {Fore.RED}3. {Fore.WHITE}Descrição do jogo                                    |")
-    print(f"| {Fore.RED}4. {Fore.WHITE}Sair                                                 |")
-    print("|                                                         |")
-    print("| / / / / / / / / / / / / / / / / / / / / / / / / / / / / |")
-    print("\nIntroduza uma opção: ")
-    op = int(input())
-
-    # check if user input is an acceptable answer
-    while op <= 0 or op >= 5:
+    while True:         # loops until user provides a valid input
+        # menu creation
         os.system('cls')
-        print("Oops... Opção inválida!")
-        time.sleep(1)
-        input("\nPressione uma tecla para voltar...")
-        showMenu()
-       
+        print(f"| / / / / / / / / / /{Fore.GREEN}🍀Lucky Numbers🍀{Fore.WHITE}/ / / / / / / / / / |")
+        print(f"| / / / / / / / / / /       MENU      / / / / / / / / / / |")
+        print("|                                                         |")
+        print(f"| {Fore.RED}1. {Fore.WHITE}Iniciar uma partida                                  |")
+        print(f"| {Fore.RED}2. {Fore.WHITE}Carregar uma partida a partir de um ficheiro         |")
+        print(f"| {Fore.RED}3. {Fore.WHITE}Descrição do jogo                                    |")
+        print(f"| {Fore.RED}4. {Fore.WHITE}Sair                                                 |")
+        print("|                                                         |")
+        print("| / / / / / / / / / / / / / / / / / / / / / / / / / / / / |")
+        op1 = input("Introduza uma opção: ")
+        if op1 in ['1','2','3','4']:
+            op = int(op1)
+            break  # Break out of the loop if a valid number is input
+        
     # runs program based on the user's input
     if op == 1:
-        os.system('cls')
-        player1 = input("Digite o nome do jogador: ")
-        runGame(player1, player2, board1, board2)
-        showMenu()
+        x = chooseGame()
+        if x == 1:
+            player1 = input(f"{Fore.GREEN}Digite o nome do primeiro jogador: ")
+            player2 = input(f"{Fore.GREEN}Digite o nome do segundo jogador: ")
+            twoPlayerGame(player1, player2, board1, board2)
+        else:
+            player1 = input(f"{Fore.GREEN}Digite o nome do primeiro jogador: ")
+            
     elif op == 2:
         os.system('exit')
     elif op == 3:
@@ -61,9 +73,8 @@ def showMenu():
         showMenu()
     elif op == 4:
         os.system('cls')
-        print("Sessão Terminada!")
+        print(f"{Fore.RED}Sessão Terminada!")
         os.system('exit')
-
 
 # calling function to start LuckyNumbers 
 showPage()
